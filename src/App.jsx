631268b492f9f215a89c8c958bbb0e71e51dad4f
NME.eth <maximus.holm@gmail.com>
2022-05-27 22:52:20 +0300

    completed webapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,59 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
 
+  const { search, animals } = useAnimalSearch();
+
   return (
-    <div>
-    </div>
+    <main>
+      <h1>Animal Farm</h1>
+      <input
+        type="text"
+        placeholder="Search"
+        value={localStorage.getItem('lastQuery')}
+        onChange={(e) => search(e.target.value)}
+      />
+
+      <ul>
+        {animals.map((animal) => (
+          <Animal key={animal.id} {...animal} />
+        ))}
+        {animals.length === 0 && 'No animals found D:'}
+      </ul>
+    </main>
   )
+}
+
+function Animal({ type, name, age }) {
+  return (
+    <li>
+      <strong>{type}</strong> {name} ({age} years old)
+    </li>
+  );
+
+}
+
+function useAnimalSearch() {
+  const [animals, setAnimals] = useState([]);
+
+  const search = async (q) => {
+    const response = await fetch(
+      'http://localhost:8080?' + new URLSearchParams({ q })
+    );
+    const data = await response.json();
+
+    setAnimals(data);
+
+    localStorage.setItem('lastQuery', q);
+  }
+
+  useEffect(() => {
+    const lastQuery = localStorage.getItem('lastQuery');
+    search(lastQuery);
+  }, []);
+
+  return { search, animals };
 }
 
 export default App
