c58365c0f559ade1f79adf2633c8f63bff4b0c69
NME.eth <maximus.holm@gmail.com>
2022-06-03 23:18:17 +0300

    sign in with google

--- miniblog/pages/enter.tsx
+++ miniblog/pages/enter.tsx
@@ -2,11 +2,31 @@ import {
   Text,
   Container,
 } from "@chakra-ui/react";
+import { auth, googleAuthProvider } from '../lib/firebase';
 
-export default function Page({ }) {
+export default function Enter({ }) {
+  const user = null;
+  const username = null;
+
+  // 1. User signed out <SignInButton />
+  // 2. User signed in, but missing username <UsernameForm />
+  // 3. User signed in, has username <SignOutButton />
   return (
     <Container>
       <Text color="blue.500" fontSize='xl'>Sign Up</Text>
     </Container>
   );
+}
+
+// Sign in with Google button
+function SignInButton() {
+}
+
+// Sign out button
+function SignOutButton() {
+}
+
+// Allow user to set their username
+function UsernameForm() {
+
 }
